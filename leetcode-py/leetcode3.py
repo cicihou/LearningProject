e3fc7aaa49568f2e0409a0cc2e14f6f85920a193
cicihou <houxi_zuel@163.com>
2021-05-31 10:56:34 +0800

    add: leetcode

--- leetcode-py/leetcode3.py
+++ leetcode-py/leetcode3.py
@@ -1,26 +1,40 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # li = []
+        '''
+        method 1 brute force
+        time O(n^2)
+        space O(n)
+        '''
+        # count = 0
+        # if s == '':
+        #     return 0
+        #
+        # for i, letter in enumerate(s):
+        #     li = [letter]
+        #     for j in s[i+1:]:
+        #         if j not in li:
+        #             li.append(j)
+        #         else:
+        #             break
+        #     count = max(count, len(li))
+        #
+        # return count
 
-        count = 0
-        if s == '':
-            return 0
+        ''' method 2 hash 
+        time O(n)
+        space O(n)
+        '''
+        used = {}
+        max_length = start = 0
+        for i, c in enumerate(s):
+            if c in used and start <= used[c]:
+                start = used[c] + 1
+            else:
+                max_length = max(max_length, i - start + 1)
 
-        for i, letter in enumerate(s):
-            li = [letter]
-            # dic = {}
-            for j in s[i+1:]:
-                if j not in li:
-                    li.append(j)
-                else:
-                    break
-            count = max(count, len(li))
+            used[c] = i
+        return max_length
 
-        return count
 
 s = Solution()
 print(s.lengthOfLongestSubstring("abcabcbb"))
